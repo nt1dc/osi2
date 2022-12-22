6e8a0554a033a16de80b5fb15e37b087278a9ef7
nt1dc <nt1dcare@gmail.com>
2022-12-22 17:08:54 +0300

    fixes

--- lab_km.c
+++ lab_km.c
@@ -19,42 +19,65 @@ MODULE_AUTHOR("Uzbek");
 MODULE_DESCRIPTION("OS LAB2");
 
 static int lab_dev_open(struct inode *inode, struct file *file);
+
 static int lab_dev_release(struct inode *inode, struct file *file);
-static ssize_t lab_dev_read(struct file *filp, char __user *buf, size_t len, loff_t *off);
+
+static ssize_t lab_dev_read(struct file *filp, char __user
+
+*buf,
+size_t len, loff_t
+*off);
+
 static ssize_t lab_dev_write(struct file *filp, const char *buf, size_t len, loff_t *off);
+
 static long lab_dev_ioctl(struct file *file, unsigned int cmd, unsigned long arg);
+
 /*
 ** This function will be called when we open the Device file
 */
-static int lab_dev_open(struct inode *inode, struct file *file)
-{
-    printk(KERN_INFO "Device File Opened...!!!\n");
+static int lab_dev_open(struct inode *inode, struct file *file) {
+    printk(KERN_INFO
+    "Device File Opened...!!!\n");
     return 0;
 }
+
 /*
 ** This function will be called when we close the Device file
 */
-static int lab_dev_release(struct inode *inode, struct file *file)
-{
+static int lab_dev_release(struct inode *inode, struct file *file) {
 
-    printk(KERN_INFO "Device File Closed...!!!\n");
+    printk(KERN_INFO
+    "Device File Closed...!!!\n");
     return 0;
 }
+
 /*
 ** This function will be called when we read the Device file
 */
-static ssize_t lab_dev_read(struct file *filp, char __user *buf, size_t len, loff_t *off)
+static ssize_t lab_dev_read(struct file *filp, char __user
+
+*buf,
+size_t len, loff_t
+*off)
 {
-printk(KERN_INFO "Read Function\n");
+printk(KERN_INFO
+"Read Function\n");
 return 0;
 }
+
 /*
 ** This function will be called when we write the Device file
 */
-static ssize_t lab_dev_write(struct file *filp, const char __user *buf, size_t len, loff_t *off)
+static ssize_t lab_dev_write(struct file *filp, const char __user
+
+*buf,
+size_t len, loff_t
+*off)
 {
-printk(KERN_INFO "Write function\n");
-return len;
+printk(KERN_INFO
+"Write function\n");
+return
+len;
 }
 
 #if (LINUX_VERSION_CODE < KERNEL_VERSION(2, 6, 35))
@@ -63,45 +86,65 @@ static int device_ioctl(struct inode *inode,
                         unsigned int ioctl_num,
                         unsigned long ioctl_param)
 #else
+
 static long lab_dev_ioctl(struct file *file, unsigned int ioctl_num, unsigned long ioctl_param)
 #endif
 {
-    printk(KERN_INFO "lab_dev_ioctl(%p,%lu,%lu)", file, ioctl_num, ioctl_param);
+    printk(KERN_INFO
+    "lab_dev_ioctl(%p,%lu,%lu)", file, ioctl_num, ioctl_param);
 
     if (ioctl_num == IOCTL_GET_VM_AREA_STRUCT)
     {
 
         struct vm_area_struct_info *vasi = vmalloc(sizeof(struct vm_area_struct_info));
-        copy_from_user(vasi, (struct vm_area_struct_info *)ioctl_param, sizeof(struct vm_area_struct_info));
+        copy_from_user(vasi, (struct vm_area_struct_info *) ioctl_param, sizeof(struct vm_area_struct_info));
         struct task_struct *task;
 
         task = get_pid_task(find_get_pid(vasi->pid), PIDTYPE_PID);
-        if (task == NULL)
-        {
-            pr_err("Process with <PID> = %d doesn't exist\n",vasi->pid);
+        if (task == NULL) {
+            pr_err("Process with <PID> = %d doesn't exist\n", vasi->pid);
             return 1;
         }
 
-        if (task->mm == NULL)
-        {
-            printk(KERN_INFO "Can't find vm_area_struct with this pid\n");
+        if (task->mm == NULL) {
+            printk(KERN_INFO
+            "Can't find vm_area_struct with this pid\n");
             return 2;
         }
         struct mm_struct *mm;
         struct vm_area_struct *vm_area;
-        printk(KERN_INFO "vm area struct\n");
+        printk(KERN_INFO
+        "vm area struct\n");
         struct vm_area_struct *pos = NULL;
         int i = 0;
-        for (pos = task->mm->mmap, i = 0; pos != NULL && i<MAX_COUNT_VM_AREA_STRUCTES; pos = pos->vm_next, i++)
-        {
+        for (pos = task->mm->mmap, i = 0; pos != NULL && i < MAX_COUNT_VM_AREA_STRUCTES; pos = pos->vm_next, i++) {
             vasi->vapi[i].permissions = pos->vm_flags;
             vasi->vapi[i].vm_start = pos->vm_start;
             vasi->vapi[i].vm_end = pos->vm_end;
             vasi->vapi[i].rb_subtree_gap = pos->rb_subtree_gap;
         }
         vasi->actual_count = i - 1;
-        copy_to_user((struct vm_area_struct_info *)ioctl_param, vasi, sizeof(struct vm_area_struct_info));
+        copy_to_user((struct vm_area_struct_info *) ioctl_param, vasi, sizeof(struct vm_area_struct_info));
         vfree(vasi);
+    }
+    if(ioctl_num==IOCTL_GET_VM_AREA_STRUCT){
+        struct lab_signal_struct_data *lsigsd = vmalloc(sizeof(struct lab_signal_struct_data));
+        copy_from_user(lsigsd, (struct lab_signal_struct_data *)ioctl_param, sizeof(struct lab_signal_struct_data));
+        t = get_pid_task(find_get_pid(lsigsd->pid), PIDTYPE_PID);
+        if (t == NULL)
+        {
+            printk(KERN_ERR "task_struct with pid=%d does not exist\n", lsigsd->pid);
+            vfree(lsigsd);
+            return 2;
+        };
+        lsigsd->result.flags = t->signal->flags;
+        lsigsd->result.group_exit_code = t->signal->group_exit_code;
+        lsigsd->result.leader = t->signal->leader;
+        lsigsd->result.notify_count = t->signal->notify_count;
+
+        lsigsd->result.nr_threads = t->signal->nr_threads;
+        copy_to_user((struct lab_signal_struct_data *)ioctl_param, lsigsd, sizeof(struct lab_signal_struct_data));
+        vfree(lsigsd);
     }
     return 0;
 }
@@ -120,20 +163,18 @@ struct file_operations file_ops =
 #endif
         };
 
-int init_module()
-{
+int init_module() {
     int ret_val;
     ret_val = register_chrdev(MAJOR_NUM, DEVICE_NAME, &file_ops);
-    if (ret_val < 0)
-    {
-        printk(KERN_ALERT "%s failed with %d\n", "Sorry, registering the character device \n", ret_val);
+    if (ret_val < 0) {
+        printk(KERN_ALERT
+        "%s failed with %d\n", "Sorry, registering the character device \n", ret_val);
         return ret_val;
     }
 
     return 0;
 }
 
-void cleanup_module()
-{
+void cleanup_module() {
     unregister_chrdev(MAJOR_NUM, DEVICE_NAME);
 }
--- user_app.c
+++ user_app.c
@@ -61,5 +61,23 @@ int main(int argc, char **argv) {
         printf("\t%1d", vasi->vapi[i].rb_subtree_gap);
         printf("\n");
     }
+
+    struct lab_signal_struct_data *lsigsd = malloc(sizeof(struct lab_signal_struct_data));
+    lsigsd->pid = pid;
+
+    ret_val = ioctl(fd, IOCTL_GET_SIGNAL_INFO, lsigsd);
+    if (ret_val != 0) {
+        printf("IOCTL_GET_SYSCALL_INFO failed %d: process with <PID> = %d doesn't exist\n", ret_val, lsigsd->pid);
+        exit(ret_val);
+    }
+
+    printf("<-- SIGNAL STRUCT -->\n");
+    printf("FOR SIGNAL_STRUCT WITH PID = %d\n", lsigsd->pid);
+    printf("FLASG = %x\n", lsigsd->result.flags);
+    printf("GROUP EXIT CODE = %d\n", lsigsd->result.group_exit_code);
+    printf("LEADER = %d\n", lsigsd->result.leader);
+    printf("NOTIFY COUNT = %d\n", lsigsd->result.notify_count);
+    printf("NR THREAD = %d\n", lsigsd->result.nr_threads);
+
     return 0;
 }
