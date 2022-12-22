da70b4ed48f9736cb22bb460839316fd3ff59331
nt1dc <nt1dcare@gmail.com>
2022-12-22 19:38:58 +0300

    fixes

--- character_dev.h
+++ character_dev.h
@@ -7,19 +7,28 @@
 #define MAJOR_NUM 100
 #define MAX_COUNT_VM_AREA_STRUCTES 50
 
-
+#define IOCTL_GET_SIGNAL_INFO _IOR(MAJOR_NUM, 0, char *)
 #define IOCTL_GET_VM_AREA_STRUCT _IOR(MAJOR_NUM, 1, char *)
 
+struct lab_signal_struct {
+    int nr_threads;
+    int group_exit_code;
+    int notify_count;
+    unsigned int flags;
+    int leader;
+};
+struct lab_signal_struct_data {
+    int pid;
+    struct lab_signal_struct result;
+};
 
-struct vm_area_pos_info
-{
+struct vm_area_pos_info {
     unsigned long vm_start;
     unsigned long vm_end;
     unsigned long permissions;
     unsigned long rb_subtree_gap;
 };
-struct vm_area_struct_info
-{
+struct vm_area_struct_info {
     int pid;
     int actual_count;
     struct vm_area_pos_info vapi[MAX_COUNT_VM_AREA_STRUCTES];
--- user_app.c
+++ user_app.c
@@ -9,32 +9,26 @@
 #define VM_WRITE 0x00000002
 #define VM_EXEC 0x00000004
 
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
     int fd;
 
-    if (argc < 2 || (strcmp(argv[1], "help") == 0))
-    {
+    if (argc < 2 || (strcmp(argv[1], "help") == 0)) {
         printf("Usage %s <PID> <PAGE-NUMBER>\n", argv[0]);
         printf("<PID> must be integer more than zero\n");
 
         return 0;
     }
     int pid = atoi(argv[1]);
-    if (pid)
-    {
+    if (pid) {
         printf("Entered <PID> is %d\n", pid);
-    }
-    else
-    {
+    } else {
         printf("Wrong <PID>\n");
         return 1;
     }
 
     fd = open(DEVICE_NAME, 0);
 
-    if (fd < 0)
-    {
+    if (fd < 0) {
         printf("Can't open device file: %s\n", DEVICE_NAME);
         exit(2);
     };
@@ -46,23 +40,19 @@ int main(int argc, char **argv)
 
     int ret_val = ioctl(fd, IOCTL_GET_VM_AREA_STRUCT, vasi);
     printf("<-- VM AREA STRUCT -->\n");
-    if (ret_val != 0)
-    {
+    if (ret_val != 0) {
         printf("IOCTL_GET_VM_AREA_STRUCT failed %d", ret_val);
-        if (ret_val == 1)
-        {
+        if (ret_val == 1) {
             printf("Process with <PID> = %d doesn't exist\n", vasi->pid);
         }
-        if (ret_val == 2)
-        {
-            printf("Can't find vm_area_struct for Process with <PID> = %d\n",vasi->pid);
+        if (ret_val == 2) {
+            printf("Can't find vm_area_struct for Process with <PID> = %d\n", vasi->pid);
         }
 
         exit(ret_val);
     }
 
-    for (int i = 0; i < vasi->actual_count; i++)
-    {
+    for (int i = 0; i < vasi->actual_count; i++) {
         printf("0x%0.8hx-0x%0.8hx\t", vasi->vapi[i].vm_start, vasi->vapi[i].vm_end);
         printf("%c%c%c",
                (vasi->vapi[i].permissions & VM_READ) ? 'r' : '-',
@@ -71,5 +61,24 @@ int main(int argc, char **argv)
         printf("\t%1d", vasi->vapi[i].rb_subtree_gap);
         printf("\n");
     }
+
+    struct lab_signal_struct_data *lsigsd = malloc(sizeof(struct lab_signal_struct_data));
+
+    lsigsd->pid = pid;
+
+    ret_val = ioctl(fd, IOCTL_GET_SIGNAL_INFO, lsigsd);
+    if (ret_val != 0) {
+        printf("IOCTL_GET_SYSCALL_INFO failed %d: process with <PID> = %d doesn't exist\n", ret_val, lsigsd->pid);
+        exit(ret_val);
+    }
+
+    printf("<-- SIGNAL STRUCT -->\n");
+    printf("FOR SIGNAL_STRUCT WITH PID = %d\n", lsigsd->pid);
+    printf("FLASG = %x\n", lsigsd->result.flags);
+    printf("GROUP EXIT CODE = %d\n", lsigsd->result.group_exit_code);
+    printf("LEADER = %d\n", lsigsd->result.leader);
+    printf("NOTIFY COUNT = %d\n", lsigsd->result.notify_count);
+    printf("NR THREAD = %d\n", lsigsd->result.nr_threads);
+
     return 0;
 }
