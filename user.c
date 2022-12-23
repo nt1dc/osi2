ca10287fbbe41489454866ecfa747331abeba6b4
nt1dc <nt1dcare@gmail.com>
2022-12-23 16:17:01 +0300

    try to add mutex

--- user.c
+++ user.c
@@ -42,7 +42,7 @@ int main(int argc, char **argv) {
     printf("has\n");
 
     struct vm_area_pos_info *s = vasi->vapi;
-    *s = realloc(s, sizeof(struct vm_area_pos_info) * (size));
+    *s = realloc(s, sizeof(struct vm_area_pos_info) * (bsize));
 
     vasi->pid = pid;
     printf("was here \n");
