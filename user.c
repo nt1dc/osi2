4cd612d02e5431d171eda6a91a587f693c36ff19
nt1dc <nt1dcare@gmail.com>
2022-12-23 16:07:37 +0300

    try to add mutex

--- user.c
+++ user.c
@@ -10,7 +10,7 @@
 #define VM_EXEC 0x00000004
 
 void increase(int **data, int size) {
-//    *data = realloc(*data, sizeof(struct vm_area_pos_info) * (size));
+    *data = realloc(*data, sizeof(struct vm_area_pos_info) * (size));
 }
 
 int main(int argc, char **argv) {
@@ -36,57 +36,57 @@ int main(int argc, char **argv) {
     };
     struct buff_size_info *buffSizeInfo = malloc(sizeof(struct buff_size_info));
     buffSizeInfo->pid = pid;
-    int r = ioctl(fd, IOCTL_GET_BUFF_SIZE, buffSizeInfo);
-    printf("%d\n", buffSizeInfo->size);
-    int bsize = buffSizeInfo->size;
-    free(buffSizeInfo);
+//    int r = ioctl(fd, IOCTL_GET_BUFF_SIZE, buffSizeInfo);
+//    printf("%d\n", buffSizeInfo->size);
+//    int bsize = buffSizeInfo->size;
+//    free(buffSizeInfo);
+//
+//    struct vm_area_struct_info *vasi = malloc(sizeof(struct vm_area_struct_info));
+//    printf("has\n");
+//
+//    struct vm_area_pos_info *s = vasi->vapi;
+//    increase(&s, bsize);
+//    vasi->pid = pid;
+//    printf("was here \n");
+//    int ret_val = ioctl(fd, IOCTL_GET_VM_AREA_STRUCT, vasi);
+//    printf("<-- VM AREA STRUCT -->\n");
+//    if (ret_val != 0) {
+//        printf("IOCTL_GET_VM_AREA_STRUCT failed %d", ret_val);
+//        if (ret_val == 1) {
+//            printf("Process with <PID> = %d doesn't exist\n", vasi->pid);
+//        }
+//        if (ret_val == 2) {
+//            printf("Can't find vm_area_struct for Process with <PID> = %d\n", vasi->pid);
+//        }
+//        exit(ret_val);
+//    }
+//
+//    for (int i = 0; i < vasi->actual_count; i++) {
+//        printf("0x%0.8hx-0x%0.8hx\t", vasi->vapi[i].vm_start, vasi->vapi[i].vm_end);
+//        printf("%c%c%c",
+//               (vasi->vapi[i].permissions & VM_READ) ? 'r' : '-',
+//               (vasi->vapi[i].permissions & VM_WRITE) ? 'w' : '-',
+//               (vasi->vapi[i].permissions & VM_EXEC) ? 'x' : '-');
+//        printf("\t%1d\n", vasi->vapi[i].rb_subtree_gap);
+//    }
 
-    struct vm_area_struct_info *vasi = malloc(sizeof(struct vm_area_struct_info));
-    printf("has\n");
-
-    struct vm_area_pos_info *s = vasi->vapi;
-    increase(&s, bsize);
-    vasi->pid = pid;
-    printf("was here \n");
-    int ret_val = ioctl(fd, IOCTL_GET_VM_AREA_STRUCT, vasi);
-    printf("<-- VM AREA STRUCT -->\n");
-    if (ret_val != 0) {
-        printf("IOCTL_GET_VM_AREA_STRUCT failed %d", ret_val);
-        if (ret_val == 1) {
-            printf("Process with <PID> = %d doesn't exist\n", vasi->pid);
-        }
-        if (ret_val == 2) {
-            printf("Can't find vm_area_struct for Process with <PID> = %d\n", vasi->pid);
-        }
-        exit(ret_val);
-    }
-
-    for (int i = 0; i < vasi->actual_count; i++) {
-        printf("0x%0.8hx-0x%0.8hx\t", vasi->vapi[i].vm_start, vasi->vapi[i].vm_end);
-        printf("%c%c%c",
-               (vasi->vapi[i].permissions & VM_READ) ? 'r' : '-',
-               (vasi->vapi[i].permissions & VM_WRITE) ? 'w' : '-',
-               (vasi->vapi[i].permissions & VM_EXEC) ? 'x' : '-');
-        printf("\t%1d\n", vasi->vapi[i].rb_subtree_gap);
-    }
-
-    struct lab_signal_struct_data *lsigsd = malloc(sizeof(struct lab_signal_struct_data));
-
-    lsigsd->pid = pid;
-
-     ret_val = ioctl(fd, IOCTL_GET_SIGNAL_INFO, lsigsd);
-    if (ret_val != 0) {
-        printf("IOCTL_GET_SYSCALL_INFO failed %d: process with <PID> = %d doesn't exist\n", ret_val, lsigsd->pid);
-        exit(ret_val);
-    }
-
-    printf("<-- SIGNAL STRUCT -->\n");
-    printf("FOR SIGNAL_STRUCT WITH PID = %d\n", lsigsd->pid);
-    printf("FLASG = %x\n", lsigsd->result.flags);
-    printf("GROUP EXIT CODE = %d\n", lsigsd->result.group_exit_code);
-    printf("LEADER = %d\n", lsigsd->result.leader);
-    printf("NOTIFY COUNT = %d\n", lsigsd->result.notify_count);
-    printf("NR THREAD = %d\n", lsigsd->result.nr_threads);
+//    struct lab_signal_struct_data *lsigsd = malloc(sizeof(struct lab_signal_struct_data));
+//
+//    lsigsd->pid = pid;
+//
+//     ret_val = ioctl(fd, IOCTL_GET_SIGNAL_INFO, lsigsd);
+//    if (ret_val != 0) {
+//        printf("IOCTL_GET_SYSCALL_INFO failed %d: process with <PID> = %d doesn't exist\n", ret_val, lsigsd->pid);
+//        exit(ret_val);
+//    }
+//
+//    printf("<-- SIGNAL STRUCT -->\n");
+//    printf("FOR SIGNAL_STRUCT WITH PID = %d\n", lsigsd->pid);
+//    printf("FLASG = %x\n", lsigsd->result.flags);
+//    printf("GROUP EXIT CODE = %d\n", lsigsd->result.group_exit_code);
+//    printf("LEADER = %d\n", lsigsd->result.leader);
+//    printf("NOTIFY COUNT = %d\n", lsigsd->result.notify_count);
+//    printf("NR THREAD = %d\n", lsigsd->result.nr_threads);
 
     return 0;
 }
