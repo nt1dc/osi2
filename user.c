fdd73f1ad454d678b114605b537ecd53c64389ff
nt1dc <nt1dcare@gmail.com>
2022-12-23 14:39:18 +0300

    try to add mutex

--- user.c
+++ user.c
@@ -36,7 +36,7 @@ int main(int argc, char **argv) {
     printf("%s", buffSizeInfo->size);
 
     struct vm_area_struct_info *vasi = malloc(sizeof(struct vm_area_struct_info));
-    vasi->vapi = realloc(vasi->vapi, struct vm_area_pos_info vapi[buffSizeInfo->size]);
+    vasi->vapi = realloc(vasi->vapi, sizeof (struct vm_area_pos_info vapi[buffSizeInfo->size]));
     vasi->pid = pid;
 
     int ret_val = ioctl(fd, IOCTL_GET_VM_AREA_STRUCT, vasi);
