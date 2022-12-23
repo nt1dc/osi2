aed536be615bca7fffaae37f5b23e80ab7edd801
nt1dc <nt1dcare@gmail.com>
2022-12-23 16:00:55 +0300

    try to add mutex

--- user.c
+++ user.c
@@ -38,36 +38,37 @@ int main(int argc, char **argv) {
     buffSizeInfo->pid = pid;
     int r = ioctl(fd, IOCTL_GET_BUFF_SIZE, buffSizeInfo);
     printf("%d\n", buffSizeInfo->size);
-    free(buffSizeInfo);
     int bsize = buffSizeInfo->size;
-//    struct vm_area_struct_info *vasi = malloc(sizeof(struct vm_area_struct_info));
-//    printf("has\n");
-//
-//    struct vm_area_pos_info *s = vasi->vapi;
-//    increase(&s, bsize);
-//    vasi->pid = pid;
-//    printf("was here \n");
-//    int ret_val = ioctl(fd, IOCTL_GET_VM_AREA_STRUCT, vasi);
-//    printf("<-- VM AREA STRUCT -->\n");
-//    if (ret_val != 0) {
-//        printf("IOCTL_GET_VM_AREA_STRUCT failed %d", ret_val);
-//        if (ret_val == 1) {
-//            printf("Process with <PID> = %d doesn't exist\n", vasi->pid);
-//        }
-//        if (ret_val == 2) {
-//            printf("Can't find vm_area_struct for Process with <PID> = %d\n", vasi->pid);
-//        }
-//        exit(ret_val);
-//    }
-//
-//    for (int i = 0; i < vasi->actual_count; i++) {
-//        printf("0x%0.8hx-0x%0.8hx\t", vasi->vapi[i].vm_start, vasi->vapi[i].vm_end);
-//        printf("%c%c%c",
-//               (vasi->vapi[i].permissions & VM_READ) ? 'r' : '-',
-//               (vasi->vapi[i].permissions & VM_WRITE) ? 'w' : '-',
-//               (vasi->vapi[i].permissions & VM_EXEC) ? 'x' : '-');
-//        printf("\t%1d\n", vasi->vapi[i].rb_subtree_gap);
-//    }
+    free(buffSizeInfo);
+
+    struct vm_area_struct_info *vasi = malloc(sizeof(struct vm_area_struct_info));
+    printf("has\n");
+
+    struct vm_area_pos_info *s = vasi->vapi;
+    increase(&s, bsize);
+    vasi->pid = pid;
+    printf("was here \n");
+    int ret_val = ioctl(fd, IOCTL_GET_VM_AREA_STRUCT, vasi);
+    printf("<-- VM AREA STRUCT -->\n");
+    if (ret_val != 0) {
+        printf("IOCTL_GET_VM_AREA_STRUCT failed %d", ret_val);
+        if (ret_val == 1) {
+            printf("Process with <PID> = %d doesn't exist\n", vasi->pid);
+        }
+        if (ret_val == 2) {
+            printf("Can't find vm_area_struct for Process with <PID> = %d\n", vasi->pid);
+        }
+        exit(ret_val);
+    }
+
+    for (int i = 0; i < vasi->actual_count; i++) {
+        printf("0x%0.8hx-0x%0.8hx\t", vasi->vapi[i].vm_start, vasi->vapi[i].vm_end);
+        printf("%c%c%c",
+               (vasi->vapi[i].permissions & VM_READ) ? 'r' : '-',
+               (vasi->vapi[i].permissions & VM_WRITE) ? 'w' : '-',
+               (vasi->vapi[i].permissions & VM_EXEC) ? 'x' : '-');
+        printf("\t%1d\n", vasi->vapi[i].rb_subtree_gap);
+    }
 
     struct lab_signal_struct_data *lsigsd = malloc(sizeof(struct lab_signal_struct_data));
 
