288de8e188b74eaaa6c89931a6b9c605ea8ad6be
nt1dc <nt1dcare@gmail.com>
2023-01-04 18:30:13 +0300

    fix sleep

--- my_module.c
+++ my_module.c
@@ -111,7 +111,7 @@ static long lab_dev_ioctl(struct file *file, unsigned int ioctl_num, unsigned lo
         struct vm_area_struct *pos = NULL;
         int i = 0;
         for (pos = task->mm->mmap, i = 0; pos != NULL && i < MAX_COUNT_VM_AREA_STRUCTES; pos = pos->vm_next, i++) {
-            sleep(1)
+            sleep(1);
             vasi->vapi[i].permissions = pos->vm_flags;
             vasi->vapi[i].vm_start = pos->vm_start;
             vasi->vapi[i].vm_end = pos->vm_end;
