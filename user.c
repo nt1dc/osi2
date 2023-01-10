2eebbb873bd6fe864b2cc4719cac612ad359046b
nt1dc <nt1dcare@gmail.com>
2023-01-10 23:53:59 +0300

    try to add dynamic buffer

--- user.c
+++ user.c
@@ -35,10 +35,9 @@ int main(int argc, char **argv) {
     int r = ioctl(fd, IOCTL_GET_BUFFER_SIZE,bufferSizeStructInfo);
     printf("%d \n", bufferSizeStructInfo->size);
 
-    struct vm_area_struct_info *vasi = malloc(sizeof(struct vm_area_struct_info));
+    struct vm_area_struct_info *vasi = malloc(sizeof(struct vm_area_struct_info)+ sizeof( struct vm_area_pos_info)*bufferSizeStructInfo->size );
 //    читать тут https://stackoverflow.com/questions/22179161/invalid-use-of-flexible-array-member
 //    и в принципе все будет доне )
-    vasi->vapi = realloc(vasi->vapi, sizeof( struct vm_area_pos_info)*bufferSizeStructInfo->size);
 
     vasi->pid = pid;
 
